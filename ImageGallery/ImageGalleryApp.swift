6ea260d022ffb759891e0bfed2b42557898336b2
Alex <oleksii.pidhornyi@gmail.com>
2024-12-24 13:52:44 +0100

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Oleksii on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
